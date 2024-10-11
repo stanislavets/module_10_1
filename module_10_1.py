fbfa3cd0a1c9a064210f47e46bc89340d1e92c5e
stanislavets <stanislavets2009@mail.ru>
2024-10-11 12:42:21 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,54 @@
+# Импорты необходимых модулей и функций
+import requests
+from time import sleep
+from datetime import datetime
+from threading import Thread
+
+# Объявление функции write_words
+def write_words(word_count, file_name): # где word_count - количество записываемых слов,
+    file = open(file_name, 'a', encoding='utf-8')         # file_name - название файла, куда будут записываться слова.
+    for i in range(word_count):
+        file.write( f'Какое-то слово №  {i+1}\n')
+        sleep(0.1)
+    file.close()
+    print(f'Завершилась запись в файл {file_name}')
+
+# Взятие текущего времени
+time_start = datetime.now()
+# Запуск функций с аргументами из задачи
+write_words(10, 'example1.txt')       # После создания файла вызовите 4 раза функцию wite_words
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+# Взятие текущего времени
+time_stop = datetime.now()
+time_res = time_stop - time_start
+# Вывод разницы начала и конца работы функций
+print(f'Время работы функций {time_res}')
+
+# После вызовов функций создайте 4 потока для вызова этой функции
+time2_start = datetime.now()    # Взятие текущего времени
+# Создание и запуск потоков с аргументами из задачи
+thr_first = Thread(target=write_words, args= (10, 'example5.txt'))
+thr_second = Thread(target=write_words, args= (30, 'example6.txt'))
+thr_third = Thread(target=write_words, args= (200, 'example7.txt'))
+thr_fourh = Thread(target=write_words, args= (100, 'example8.txt'))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourh.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourh.join()
+
+# Взятие текущего времени
+
+time2_stop = datetime.now()
+time2_res = time2_stop - time2_start
+print(f'Время работы потоков {time2_res}')
+# Вывод разницы начала и конца работы потоков
+print(f'Использование Потоков быстрее функций на {time_res-time2_res} секунд')
